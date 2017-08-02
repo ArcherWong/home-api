a55383a6edfb09edbc680d99e6c6918eb1e00e92
wangjiechang <wangjiechang@duc.cn>
2017-08-02 15:22:37 +0800

    change package.json

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.on('error', (err, ctx) => {
   log.error('server error', err, ctx)
 });
 
-app.listen(3000);
-
-console.log('listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`${pkg.name} listening on port ${port}`);
+});
